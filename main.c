10065e7f2d19a768b914deb2f3d34ce312b2dbfd
liupurnomo <liu.purnomo@gmail.com>
2020-09-30 14:16:25 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,33 @@
+/*File : main.c*/
+/*Penulis : Liu Purnomo, email liupurnomo@gmail.com*/
+/*Program menuliskan puisi berjudul "aku" karya "Chairil Anwar")*/
+
+#include <stdio.h>
+int
+main(void)
+{ /*Puisi Aku*/
+  printf(
+    "\n"
+    "Judul : AKU\n"
+    "Karya : Chairil Anwar\n"
+    "\n"
+    "Kalau sampai waktuku\n"
+    "'ku mau tak seorang kan merayu\n"
+    "Tidak juga kau\n"
+    "\n"
+    "tak perlu sedu sedan itu\n"
+    "\n"
+    "Aku ini binatang jalang\n"
+    "Dari kumpulannya terbuang\n"
+    "Biar peluru menembus kulitku\n"
+    "Aku tetap meradang menerjang\n"
+    "Luka dan bisa kubawa berlari\n"
+    "Berlari\n"
+    "Hingga hilang pedih peri\n"
+    "\n"
+    "Dan aku akan lebih tidak perduli\n"
+    "\n"
+    "Aku mau hidup seribu tahun lagi\n"
+  );
+  return 0;
+}
